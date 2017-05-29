897f8e0294a405da0c41621bd8571f5f4d29ef6b
Takayuki Kaisen <ksn0215@gmail.com>
2017-05-29 21:14:20 +0900

    a

--- lec3.scala
+++ lec3.scala
@@ -6,8 +6,22 @@
 
 //　1.Data-Parallel Programming
 
-// task-parallell programming: A form of parallelization that distributes execution processes across computing nodes
+// task-parallel programming: A form of parallelization that distributes execution processes across computing nodes
+// data-parallel programming: A form of parallelization that distributes data across computing nodes
 
+// Example: Mandelbrot Set
+
+private def computePixel(xc: Double, yc: Double, maxIterations: Int): Int = {
+	var i = 0
+	var x,y = 0.0
+	while(x * x + y * y < 4 && i < maxIterations){
+		val xt = x * x - y * y + xc
+		val yt = 2 * x * y + yc
+		x = xt; y = yt
+		i += 1
+	}
+	color(i) // this method is not defined
+}
 
 
 // 2. Data-Parallel Operations-1
